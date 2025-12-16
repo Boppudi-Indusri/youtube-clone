7eb2be0b35f4193773d57ffe44f9fa6b676667a8
Boppudi-Indusri <92775014+Boppudi-Indusri@users.noreply.github.com>
2025-12-16 09:43:24 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/auth.routes.js";
+import videoRoutes from "./routes/video.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.listen(5000, () => console.log("Server running on 5000"));
