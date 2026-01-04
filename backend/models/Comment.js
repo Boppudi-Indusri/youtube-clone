77b8c39ee787b6164a7b250133cb35e31718caf2
Boppudi-Indusri <92775014+Boppudi-Indusri@users.noreply.github.com>
2026-01-04 21:51:58 +0530

    Delete backend directory

--- backend/models/Comment.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const commentSchema = new mongoose.Schema({
-  videoId: String,
-  user: String,
-  text: String
-});
-
-export default mongoose.model("Comment", commentSchema);
